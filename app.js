79f8d3756a87344d22a2e77b5c4aa47a55dbe186
ritaurbani <ritaurbani@gmail.com>
2024-12-02 17:06:19 +0100

    feat: bacheca creation

--- app.js
+++ app.js
@@ -2,3 +2,25 @@ console.log("hello");
 const express = require("express");
 const app = express();
 const port = 3000;
+
+const posts = require("./posts")
+
+app.use(express.static("public"))
+
+app.get("/", (req, res) =>{
+    res.send ("Server del mio blog")
+
+})
+
+app.get("/bacheca", (req, res) => {
+ const bacheca = {
+    posts: posts,
+    count: posts.length
+ }
+     res.json(bacheca)
+
+})
+
+app.listen(port, () =>{
+    console.log("Start")
+})
---
+++ posts.js
@@ -0,0 +1,34 @@
+const posts = [
+    {
+        titolo: "Viaggio in Italia",
+        contenuto: "Esploriamo le bellezze naturali e storiche dell'Italia.",
+        immagine: "https://www.esempio.com/italia.jpg",
+        tags: ["Italia", "viaggio"]
+    },
+    {
+        titolo: "Nuove Tecnologie",
+        contenuto: "Scopriamo le tecnologie che definiranno il futuro.",
+        immagine: "https://www.esempio.com/tecnologie.jpg",
+        tags: ["tecnologia", "futuro"]
+    },
+    {
+        titolo: "Ricette Semplici",
+        contenuto: "Un piatto veloce e gustoso per ogni occasione.",
+        immagine: "https://www.esempio.com/ricetta.jpg",
+        tags: ["cucina", "ricetta"]
+    },
+    {
+        titolo: "Mindfulness",
+        contenuto: "Tecniche per vivere il presente con maggiore consapevolezza.",
+        immagine: "https://www.esempio.com/mindfulness.jpg",
+        tags: ["benessere", "consapevolezza"]
+    },
+    {
+        titolo: "Moda 2024",
+        contenuto: "Le tendenze più in voga per la stagione autunnale.",
+        immagine: "https://www.esempio.com/moda.jpg",
+        tags: ["moda", "tendenze"]
+    }
+];
+
+module.exports = posts;
